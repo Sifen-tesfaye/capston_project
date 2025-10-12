cb1525cd553e50a9871ee92bd3c8fd74921faf78
Sifen-tesfaye <sifen.tesfaye@aastustudent.edu.et>
2025-10-12 11:06:10 -0700

    week 4: prepare for deployment

--- task_management/settings.py
+++ task_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure---b*xcq*edl*)rsngpi!5+ck0of6#+d8^c$t39k*aa%exj3v^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sifen-tesfaye.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
